3c19d70f075973f4321f14486c96cfdcede1a31e
Dalton Rick <dalton_rick@apple.com>
2021-03-15 14:25:56 -0400

    Update install logic, fix incompatible values in configs

--- macos/install.sh
+++ macos/install.sh
@@ -1,10 +1,48 @@
 #!/usr/bin/env bash
 
 # Setup
-readonly BASE_PATH="${0%/install.sh}"
+readonly BASE_PATH="$PWD"
+BACKUP=false
+BACKUPDIR="${HOME}/ConfigBackups"
+
+function usage {
+    printf "\nUsage: ${0##*/} [OPTIONS ...] -c COMMAND\n"
+    printf "\nOptions:\n"
+    printf "    -h          Displays this help message and exits\n"
+    printf "    -b          Creates backups of original files in ${BACKUPDIR}\n"
+
+    printf "\nCommands:\n"
+    printf "    all             Installs packages and deploys config files\n"
+    printf "    packages        Only installs packages\n"
+    printf "    configs         Only deploys configs\n"
+
+}
 
 cd "$BASE_PATH"
 
+while getopts ":hb" opt; do
+	case $opt in
+		h )
+			usage
+			exit 0
+			;;
+		b )
+			BACKUP=true
+			;;
+		: )
+			echo "Option ${OPTARG} requires argument" 1>&2
+			exit 1
+			;;
+		\? )
+			echo "Invalid option ${OPTARG}" 1>&2
+			;;
+	esac
+done
+shift "$((OPTIND -1))"
+
+SUBCMD="${1:-'all'}"
+
+function install_packages {
 # Install Apps / Dependencies
 ## Brew
 if ! which brew &> /dev/null; then
@@ -27,16 +65,31 @@ echo "Please install Karabiner-Elements:"
 open -a finder $karabiner_name 
 
 read -p "Press Enter when done" _
+}
 
+function install_configs {
+mkdir -p $BACKUPDIR
 # Install configs
 ## Install vimrc
+if $BACKUP; then
+	cp "${HOME}/.vimrc" "${BACKUPDIR}/vimrc"
+	cp "${HOME}/.zshrc" "${BACKUPDIR}/zshrc"
+	cp "${HOME}/.zprofile" "${BACKUPDIR}/zprofile"
+	cp "${HOME}/.config/htop/htoprc" "${BACKUPDIR}/htoprc"
+	cp "${HOME}/.config/karabiner/karabiner.json" "${BACKUPDIR}/karabiner.json"
+	cp "${HOME}/.gitconfig" "${BACKUPDIR}/gitconfig"
+	cp "${HOME}/.gitignore_global" "${BACKUPDIR}/gitignore_global"
+fi
+
 ln -sf "${BASE_PATH}/configs/vimrc" "${HOME}/.vimrc"
 mkdir -p "${HOME}/.vim_backups"
 vim +PluginInstall +qall
 
 ## Install configs
 ln -sf "${BASE_PATH}/configs/zshrc" "${HOME}/.zshrc"
+mkdir -p "${HOME}/.zshrc.d"
 ln -sf "${BASE_PATH}/configs/zprofile" "${HOME}/.zprofile"
+mkdir -p "${HOME}/.zprofile.d"
 ln -sF "${BASE_PATH}/zsh_functions" "${HOME}/.zsh_functions"
 
 ## Install htoprc
@@ -49,10 +102,27 @@ ln -sf "${BASE_PATH}/configs/karabiner.json" "${HOME}/.config/karabiner/karabine
 
 ## Install iterm2 Prefs
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -int 1
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${BASE_PATH}/configs/com.googlecode.iterm2.plist"
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "${BASE_PATH}/configs"
 
 ## Global git configs
 ln -sf "${BASE_PATH}/configs/gitconfig" "${HOME}/.gitconfig"
 ln -sf "${BASE_PATH}/configs/gitignore_global" "${HOME}/.gitignore_global"
+}
 
 
+case $SUBCMD in
+	all )
+		install_packages
+		install_configs
+		;;
+	packages )
+		install_packages
+		;;
+	configs )
+		install_configs
+		;;
+	* )
+		echo "Invalid command: $SUBCMD"
+		exit 1
+		;;
+esac
